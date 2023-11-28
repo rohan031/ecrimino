9bc8a49fbe40ed564d39d2b9fc9d24da4c000168
rohan <rohanverma031@gmail.com>
2023-11-28 11:48:26 +0530

    phone number updated

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,7 +1,7 @@
 export const contacts = {
 	phone: [
-		{ number: "+243813-657-483", display: "(+243) 813 657 483" },
 		{ number: "+243841-307-051", display: "(+243) 841 307 051" },
+		// { number: "+243841-307-051", display: "(+243) 841 307 051" },
 	],
 	email: [
 		"contact@ecrimino.com",
