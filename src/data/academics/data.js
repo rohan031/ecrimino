8f22849f39b228878f870ed304a1aab084e76442
adityamohan09 <adityamohan3786@gmail.com>
2023-12-21 22:26:20 +0530

    phdcertilink

--- src/data/academics/data.js
+++ src/data/academics/data.js
@@ -6,37 +6,37 @@ export const masters = {
 			heading: "SÉCURITÉ INTÉRIEURE",
 			text: "Expliquer les situations problèmes dans les différents secteurs de la société congolaise, qu'elles soient criminalisées ou non, ainsi que les modalités de leur gestion en tenant compte de leur complexité",
 			image: "/events/events1.WebP",
-			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
+			link: "/documents/docs/Votre%20Ma%C3%AEtrise%20en%20criminologie%20%C3%A0%20l_Universit%C3%A9%20de%20Kinshasa.pdf",
 		},
 		{
 			heading: "CRIMINOLOGIE ÉCONOMIQUE ET ENVIRONNEMENTALE",
 			text: "Analyser les formes et les enjeux de la criminalité économique et environnementale en contexte congolais en vue d'en assurer le contrôle et d'en organiser la prévention",
 			image: "/events/events1.WebP",
-			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
+			link: "/documents/docs/Votre%20Ma%C3%AEtrise%20en%20criminologie%20%C3%A0%20l_Universit%C3%A9%20de%20Kinshasa.pdf",
 		},
 		{
 			heading: "GESTION DE LA PAIX ET DES CONFLITS",
 			text: "Concevoir un mécanisme de portée nationale, régionale ou internationale susceptible de contribuer à l'instauration d'une paix durable.",
 			image: "/events/events1.WebP",
-			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
+			link: "/documents/docs/Votre%20Ma%C3%AEtrise%20en%20criminologie%20%C3%A0%20l_Universit%C3%A9%20de%20Kinshasa.pdf",
 		},
 		{
 			heading: "PROTECTION DE L’ENFANT",
 			text: "Expliquer les situations problèmes dans les différents secteurs de la société congolaise, qu'elles soient criminalisées ou non, ainsi que les modalités de leur gestion en tenant compte de leur complexité.",
 			image: "/events/events1.WebP",
-			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
+			link: "/documents/docs/Votre%20Ma%C3%AEtrise%20en%20criminologie%20%C3%A0%20l_Universit%C3%A9%20de%20Kinshasa.pdf",
 		},
 		{
 			heading: "CRIMINALISTIQUE",
 			text: "Expliquer les situations problèmes dans les différents secteurs de la société congolaise, qu'elles soient criminalisées ou non, ainsi que les modalités de leur gestion en tenant compte de leur complexité",
 			image: "/events/events1.WebP",
-			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
+			link: "/documents/docs/Votre%20Ma%C3%AEtrise%20en%20criminologie%20%C3%A0%20l_Universit%C3%A9%20de%20Kinshasa.pdf",
 		},
 		{
 			heading: "INTERVENTION CRIMINOLOGIQUE",
 			text: "Identifier et expliquer les situations problèmes dans les différents secteurs de la société congolaise, qu'elles soient criminalisées ou non, ainsi que les modalités de leur gestion en tenant compte de leurcomplexité",
 			image: "/events/events1.WebP",
-			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
+			link: "/documents/docs/Votre%20Ma%C3%AEtrise%20en%20criminologie%20%C3%A0%20l_Universit%C3%A9%20de%20Kinshasa.pdf",
 		},
 		],
 };
@@ -45,12 +45,12 @@ export const phd = {
 	heading: "PhD",
 	text: "Les missions de l'École de Criminologie de l'Université de Kinshasa sont les suivantes : Assurer un enseignement de qualité en formant des acteurs compétents capables de comprendre les situations problématiques, criminalisées ou non, et de proposer des réponses idoines permettant, notamment de développer les droits humains, l'État de droit ainsi que la gouvernance des institutions; Développer la recherche fondamentale ou appliquée dans une approche interdisciplinaire dans le champ de la criminologie; Offrir une expertise et un accompagnement aux institutions ainsi qu'aux acteurs sociaux, dans le domaine de droits humains, de l'État de droit, de la gouvernance et, plus généralement, dans le domaine de la régulation sociale, notamment à travers la consultance et le renforcement des capacités.",
 	parts: [
-		{
-			heading: "Lorem Ipsum",
-			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque semper ultrices mauris ut maximus. Maecenas arcu risus, auctor eu eros tristique, rutrum convallis purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque sit amet dolor egestas, fermentum turpis ac, mollis enim.",
-			image: "/events/events1.WebP",
-			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
-		},
+	// "	{
+	// 		heading: "Lorem Ipsum",
+	// 		text: "L’École de criminologie et le Centre de criminologie et de pathologie sociale (CCPS), qui lui est rattaché, entendent produire des recherches fondamentales et des recherches appliquées. La recherche fondamentale est assurée par l’encadrement des doctorants, par les travaux des membres de l’École,",
+	// 		image: "/events/events1.WebP",
+	// 		link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
+	// 	},"
 	],
 };
 
@@ -59,8 +59,8 @@ export const certifications = {
 	text: "Les missions de l'École de Criminologie de l'Université de Kinshasa sont les suivantes : Assurer un enseignement de qualité en formant des acteurs compétents capables de comprendre les situations problématiques, criminalisées ou non, et de proposer des réponses idoines permettant, notamment de développer les droits humains, l'État de droit ainsi que la gouvernance des institutions; Développer la recherche fondamentale ou appliquée dans une approche interdisciplinaire dans le champ de la criminologie; Offrir une expertise et un accompagnement aux institutions ainsi qu'aux acteurs sociaux, dans le domaine de droits humains, de l'État de droit, de la gouvernance et, plus généralement, dans le domaine de la régulation sociale, notamment à travers la consultance et le renforcement des capacités.",
 	parts: [
 		{
-			heading: "Why is Home Services marketing important?",
-			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque semper ultrices mauris ut maximus. Maecenas arcu risus, auctor eu eros tristique, rutrum convallis purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque sit amet dolor egestas, fermentum turpis ac, mollis enim.",
+			heading: "Formations certifiantes et renforcement des capacités",
+			text: "L’École assure également des formations certifiantes en : - Gouvernance de la sécurité ; - Gouvernance économique ; - Paix et gouvernance. Ces formations donnent lieu à la délivrance d’un Certificat d’Université.",
 			image: "/events/events1.WebP",
 			link: "/documents/docs/ECOCRIM%20DEPLIANT.pdf",
 		},
