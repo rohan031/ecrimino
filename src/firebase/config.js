8e027ef70aea31cddc352fc04de42f48eb6e2ac1
rohan <rohanverma031@gmail.com>
2024-01-04 17:31:10 +0530

    added analytics

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,3 +17,5 @@ let firebase_app =
 	getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export default firebase_app;
+
+const analytics = getAnalytics(app);
