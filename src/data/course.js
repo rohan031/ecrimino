0d17de704541fc703ae7a21f389c32c030b54196
rohan <rohanverma031@gmail.com>
2024-01-17 00:55:18 +0530

    updated course name

--- src/data/course.js
+++ src/data/course.js
@@ -1,40 +1,40 @@
 export const courses = [
 	{
 		id: "m1",
-		name: "masters 1",
+		name: "sécurité intérieure",
 	},
 	{
 		id: "m2",
-		name: "masters 2",
+		name: "criminologie économique et environnementale",
 	},
 	{
 		id: "m3",
-		name: "masters 3",
+		name: "gestion de la paix et des conflits",
 	},
 	{
 		id: "m4",
-		name: "masters 4",
+		name: "protection de l’enfant",
 	},
 	{
 		id: "m5",
-		name: "masters 5",
+		name: "criminalistique",
 	},
 	{
 		id: "m6",
-		name: "masters 6",
+		name: "intervention criminologique",
 	},
 	{
 		id: "phd1",
-		name: "phd 1",
+		name: "Recherches et Doctorat",
 	},
 ];
 
 export const courseMap = {
-	m1: "masters 1",
-	m2: "masters 2",
-	m3: "masters 3",
-	m4: "masters 4",
-	m5: "masters 5",
-	m6: "masters 6",
-	phd1: "phd 1",
+	m1: "sécurité intérieure",
+	m2: "criminologie économique et environnementale",
+	m3: "gestion de la paix et des conflits",
+	m4: "protection de l’enfant",
+	m5: "criminalistique",
+	m6: "intervention criminologique",
+	phd1: "Recherches et Doctorat",
 };
