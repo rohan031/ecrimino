be623dfa150316c36f80397b32a71c9b4bb1cf4c
rohan <rohanverma031@gmail.com>
2024-04-06 19:33:30 +0530

    docs sections

--- src/app/(homegroup)/(homeroutes)/documents/page.tsx
+++ src/app/(homegroup)/(homeroutes)/documents/page.tsx
@@ -4,21 +4,35 @@ import React, { useState, useEffect } from "react";
 import { docs } from "@/data/doc";
 import Cards from "./Cards";
 
-interface Items {
+interface Item {
 	id: string;
 	title: string;
 	link: string;
 }
 
-export default function Documents() {
-	const [items, setItems] = useState<Items[]>();
+interface Docs {
+	heading: string;
+	items: Item[];
+}
 
+export default function Documents() {
+	const [items, setItems] = useState<Docs[]>();
 	useEffect(() => {
 		setItems(docs);
 	}, []);
 
-	const cards = items?.map((item) => {
-		return <Cards key={item.id} title={item.title} link={item.link} />;
+	const sections = items?.map((section, index) => {
+		const { items, heading } = section;
+		const cards = items?.map((item) => {
+			return <Cards key={item.id} title={item.title} link={item.link} />;
+		});
+
+		return (
+			<div className="doc-section" key={heading}>
+				<h2>{heading}</h2>
+				<div className="info-page__container doc">{cards}</div>
+			</div>
+		);
 	});
 
 	return (
@@ -27,9 +41,7 @@ export default function Documents() {
 				<h1>Documents</h1>
 			</div>
 
-			<div className="container">
-				<div className="info-page__container doc">{cards}</div>
-			</div>
+			<div className="container">{sections}</div>
 		</>
 	);
 }
--- src/data/doc.js
+++ src/data/doc.js
@@ -1,99 +1,119 @@
 export const docs = [
 	{
-		id: "afsdfadsf",
-		title: "37-TAP Art_ Joël NZAMPUNGU IMBOLE _ BOLENGE ILEBOSO FABRICE.pdf",
-		link: "https://drive.google.com/file/d/1CVny2w_Gu7_FNItC6I1BNw8ENR4QHwjX/view?usp=drive_link",
+		heading: "Seminars",
+		items: [
+			{
+				id: "afsdfadsf",
+				title: "37-TAP Art_ Joël NZAMPUNGU IMBOLE _ BOLENGE ILEBOSO FABRICE.pdf",
+				link: "https://drive.google.com/file/d/1CVny2w_Gu7_FNItC6I1BNw8ENR4QHwjX/view?usp=drive_link",
+			},
+			{
+				title: "Article 7 - CADHD-DD - N°79 vol 3 - Harris TATY MANTIMA et Etienne MBUNSU BINDU.pdf",
+				link: "https://drive.google.com/file/d/1cpZoa9s7nuIMQVEMgAt6GuIuelu95WCq/view?usp=drive_link",
+				id: "afsdfadasdfsf",
+			},
+			{
+				title: "Domaine de Sciences de l_Homme et de la Société (1)_072525.pdf",
+				link: "https://drive.google.com/file/d/1DEraNy6vbfUbFXQbXjG4SzCyFEKKlj2b/view?usp=drive_link",
+				id: "afsdadsffadsf",
+			},
+			{
+				title: "ECOCRIM DEPLIANT.pdf",
+				link: "https://drive.google.com/file/d/1SWwPb1NazYkV-1J35D3ofN5AxJePuN8H/view?usp=drive_link",
+				id: "afsdfasdasdfadsf",
+			},
+			{
+				title: "Présentation du Centre de Criminologie et de Pathologie Sociale.docx",
+				link: "/documents/docs/Pr%C3%A9sentation%20du%20Centre%20de%20Criminologie%20et%20de%20Pathologie%20Sociale.docx",
+				id: "afsdasdfadsfffadsf",
+			},
+		],
 	},
 	{
-		title: "Article 7 - CADHD-DD - N°79 vol 3 - Harris TATY MANTIMA et Etienne MBUNSU BINDU.pdf",
-		link: "https://drive.google.com/file/d/1cpZoa9s7nuIMQVEMgAt6GuIuelu95WCq/view?usp=drive_link",
-		id: "afsdfadasdfsf",
-	},
-	{
-		title: "Domaine de Sciences de l_Homme et de la Société (1)_072525.pdf",
-		link: "https://drive.google.com/file/d/1DEraNy6vbfUbFXQbXjG4SzCyFEKKlj2b/view?usp=drive_link",
-		id: "afsdadsffadsf",
-	},
-	{
-		title: "ECOCRIM DEPLIANT.pdf",
-		link: "https://drive.google.com/file/d/1SWwPb1NazYkV-1J35D3ofN5AxJePuN8H/view?usp=drive_link",
-		id: "afsdfasdasdfadsf",
-	},
-	{
-		title: "Présentation du Centre de Criminologie et de Pathologie Sociale.docx",
-		link: "/documents/docs/Pr%C3%A9sentation%20du%20Centre%20de%20Criminologie%20et%20de%20Pathologie%20Sociale.docx",
-		id: "afsdasdfadsfffadsf",
-	},
-	{
-		title: "SEMINAIRE EN INTERVENTION CRIMINOLOGIQUE 2023_230921_150638.pdf",
-		link: "https://drive.google.com/file/d/1WddbiHuyK_4veuj7zr4XiWtLWV7AUdDd/view?usp=drive_link",
-		id: "afsdfadgfhdsf",
-	},
-	{
-		title: "Votre Maîtrise en criminologie à l_Université de Kinshasa.pdf",
-		link: "https://drive.google.com/file/d/188mVqtQnU4rjETFR2BSrz3oBygTSkvYV/view?usp=drive_link",
-		id: "afsdffshgfadsf",
-	},
-	{
-		title: "ECHOS VERSION FRANCAISE MAQUETTE FIN.pdf",
-		id: "afdaswewr",
-		link: "https://drive.google.com/file/d/1QVoz38drfaOF7efYgt8gm4fLqevaezf0/view?usp=drive_link",
-	},
+		heading: "Conferences",
+		items: [
+			{
+				title: "SEMINAIRE EN INTERVENTION CRIMINOLOGIQUE 2023_230921_150638.pdf",
+				link: "https://drive.google.com/file/d/1WddbiHuyK_4veuj7zr4XiWtLWV7AUdDd/view?usp=drive_link",
+				id: "afsdfadgfhdsf",
+			},
+			{
+				title: "Votre Maîtrise en criminologie à l_Université de Kinshasa.pdf",
+				link: "https://drive.google.com/file/d/188mVqtQnU4rjETFR2BSrz3oBygTSkvYV/view?usp=drive_link",
+				id: "afsdffshgfadsf",
+			},
+			{
+				title: "ECHOS VERSION FRANCAISE MAQUETTE FIN.pdf",
+				id: "afdaswewr",
+				link: "https://drive.google.com/file/d/1QVoz38drfaOF7efYgt8gm4fLqevaezf0/view?usp=drive_link",
+			},
 
-	{
-		title: "1ROLL UP BETTY-4_231107 180910.pdf",
-		id: "afdaswewq2wr",
-		link: "https://drive.google.com/file/d/1qyzzs228KyQJ6nv5LGJH9y7CqCv340sy/view",
-	},
+			{
+				title: "1ROLL UP BETTY-4_231107 180910.pdf",
+				id: "afdaswewq2wr",
+				link: "https://drive.google.com/file/d/1qyzzs228KyQJ6nv5LGJH9y7CqCv340sy/view",
+			},
 
-	{
-		title: "ECHOS CCPS FIN....pdf",
-		id: "123wasdad21",
-		link: "https://drive.google.com/file/d/1XYr_Uv6_tflfPzHirkwpfD8PKjletOZl/view?usp=drive_link",
+			{
+				title: "ECHOS CCPS FIN....pdf",
+				id: "123wasdad21",
+				link: "https://drive.google.com/file/d/1XYr_Uv6_tflfPzHirkwpfD8PKjletOZl/view?usp=drive_link",
+			},
+		],
 	},
 	{
-		title: "Décoration du Prof Dr. Raoul Kienge-Kienge Intudi.pdf",
-		id: "a;ewra;;nfads",
-		link: "https://drive.google.com/file/d/1W6T-04QJrUMymOEv-M-AsglFnjEr__-O/view?usp=drive_link",
+		heading: "Articles",
+		items: [
+			{
+				title: "Décoration du Prof Dr. Raoul Kienge-Kienge Intudi.pdf",
+				id: "a;ewra;;nfads",
+				link: "https://drive.google.com/file/d/1W6T-04QJrUMymOEv-M-AsglFnjEr__-O/view?usp=drive_link",
+			},
+			{
+				title: "Marie-France NGUO.docx",
+				id: "a;ewra;asdf;nfads",
+				link: "https://docs.google.com/document/d/1wFeqK3upnWOQEpF-AS09vmP5CLky3fQ_/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+			},
+			{
+				title: "Article 7 - CADHD-DD - N°81 vol 1 - Harris TATY MANTIMA et cie.pdf",
+				id: "a;ewra;sajk;das;nfads",
+				link: "https://drive.google.com/file/d/19OWQNNxifmCyeghfkFexaht9BHDIlovC/view?usp=drive_link",
+			},
+			{
+				title: "ARTICLE SEMINAIRE.docx",
+				id: "asdfasdfasdfa123`",
+				link: "https://docs.google.com/document/d/1R52BLeqOv-WZnL7R4HGvsB5KiWyNt4P9/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+			},
+			{
+				title: "ARTICLE ECOCRIM 005.docx",
+				id: "asdfasdfasdfa123`asdf",
+				link: "https://docs.google.com/document/d/1OM1Wl-PJ7geuflI57ZtmoL6Ya7ALKBro/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+			},
+		],
 	},
 	{
-		title: "Marie-France NGUO.docx",
-		id: "a;ewra;asdf;nfads",
-		link: "https://docs.google.com/document/d/1wFeqK3upnWOQEpF-AS09vmP5CLky3fQ_/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
-	},
-	{
-		title: "Article 7 - CADHD-DD - N°81 vol 1 - Harris TATY MANTIMA et cie.pdf",
-		id: "a;ewra;sajk;das;nfads",
-		link: "https://drive.google.com/file/d/19OWQNNxifmCyeghfkFexaht9BHDIlovC/view?usp=drive_link",
-	},
-	{
-		title: "ARTICLE SEMINAIRE.docx",
-		id: "asdfasdfasdfa123`",
-		link: "https://docs.google.com/document/d/1R52BLeqOv-WZnL7R4HGvsB5KiWyNt4P9/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
-	},
-	{
-		title: "ARTICLE ECOCRIM 005.docx",
-		id: "asdfasdfasdfa123`asdf",
-		link: "https://docs.google.com/document/d/1OM1Wl-PJ7geuflI57ZtmoL6Ya7ALKBro/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
-	},
-	{
-		title: "ARTICLE ECOCRIM.docx",
-		id: "asdfasdfasdfa123`asdfasd",
-		link: "https://docs.google.com/document/d/1a95OMhpHcFUuX_g0CC9jZJOhPssb5DQJ/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
-	},
-	{
-		title: "ARTICLE ECOCRIM2.docx",
-		id: "asdfasdfasdfa123`zcxvads",
-		link: "https://docs.google.com/document/d/1gbhhhOzKJyQmIpzD6hCttkc5VIeCipOP/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
-	},
-	{
-		title: "ARTICLE ECRIMINO4.docx",
-		id: "asdfasdfasdfa123`qwerf",
-		link: "https://docs.google.com/document/d/1ikg1pQ9Vq6D6iNRg3-_GAzJTBzDo_8is/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
-	},
-	{
-		title: "ARTICLE ECOCRIM008.docx",
-		id: "asdfasdfasdfa123`asfqfwer",
-		link: "https://docs.google.com/document/d/1pxHNl0gTnxuk3MDTZk5B8xLlw0PIrPa8/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+		heading: "Journals",
+		items: [
+			{
+				title: "ARTICLE ECOCRIM.docx",
+				id: "asdfasdfasdfa123`asdfasd",
+				link: "https://docs.google.com/document/d/1a95OMhpHcFUuX_g0CC9jZJOhPssb5DQJ/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+			},
+			{
+				title: "ARTICLE ECOCRIM2.docx",
+				id: "asdfasdfasdfa123`zcxvads",
+				link: "https://docs.google.com/document/d/1gbhhhOzKJyQmIpzD6hCttkc5VIeCipOP/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+			},
+			{
+				title: "ARTICLE ECRIMINO4.docx",
+				id: "asdfasdfasdfa123`qwerf",
+				link: "https://docs.google.com/document/d/1ikg1pQ9Vq6D6iNRg3-_GAzJTBzDo_8is/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+			},
+			{
+				title: "ARTICLE ECOCRIM008.docx",
+				id: "asdfasdfasdfa123`asfqfwer",
+				link: "https://docs.google.com/document/d/1pxHNl0gTnxuk3MDTZk5B8xLlw0PIrPa8/edit?usp=drive_link&ouid=106564513176573800492&rtpof=true&sd=true",
+			},
+		],
 	},
 ];
