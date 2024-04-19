ce27438310dacb24cb365a1e36bb3b9b9dfdc174
rohan <rohanverma031@gmail.com>
2024-04-19 14:29:31 +0530

    removed public

--- src/app/(homegroup)/(homeroutes)/ccps/about-us/page.tsx
+++ src/app/(homegroup)/(homeroutes)/ccps/about-us/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { ccps as aboutUs } from "@/data/about-us/data";
+// import { ccps as aboutUs } from "@/data/about-us/data";
 import Heading from "../../academics/components/Heading";
-import AboutUs from "@/components/about-us/AboutUs";
+// import AboutUs from "@/components/about-us/AboutUs";
 import Content from "../component/Content";
 
 export default function Aboutus() {
-	const items = aboutUs.map((item) => {
-		return <AboutUs key={item.id} data={item} />;
-	});
+	// const items = aboutUs.map((item) => {
+	// 	return <AboutUs key={item.id} data={item} />;
+	// });
 
 	return (
 		<>
