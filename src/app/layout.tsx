13e949c4b6a4c8ff68d4a397d5b573f1066c4143
rohan <rohanverma031@gmail.com>
2024-11-22 15:52:55 +0530

    feat: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,28 @@ import { Inter } from "next/font/google";
 import "../styles/main.scss";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 config.autoAddCss = false;
 
 const inter = Inter({
-    subsets: ["latin"],
-    display: "swap",
+	subsets: ["latin"],
+	display: "swap",
 });
 
 export const metadata: Metadata = {
-    title: "Ecole de Criminologie",
-    description: "located in Democratic Republic of the Congo",
+	title: "Ecole de Criminologie",
+	description: "located in Democratic Republic of the Congo",
 };
 
 interface RootLayoutProps {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <html lang="en" className={inter.className}>
-            <body>{children}</body>
-        </html>
-    );
+	return (
+		<html lang="en" className={inter.className}>
+			<body>{children}</body>
+			<GoogleAnalytics gaId="G-JNBVBFSCTG" />
+		</html>
+	);
 }
